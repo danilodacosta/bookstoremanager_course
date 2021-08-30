4d66077b44b5f62941f4974214304c0b98c9113f
Danilo Pereira <danilo.pereira@desenvolvecidade.com>
2021-08-30 02:14:16 -0300

    Add swagger configuration.

--- src/main/java/com/danilodacosta/bookstoremanager/controller/BookController.java
+++ src/main/java/com/danilodacosta/bookstoremanager/controller/BookController.java
@@ -1,5 +1,8 @@
 package com.danilodacosta.bookstoremanager.controller;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class BookController {
 
     @GetMapping
+    @ApiOperation("Return an example hello world")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Success method return")
+    })
     public String hello() {
         return "Hello book store manager";
     }
