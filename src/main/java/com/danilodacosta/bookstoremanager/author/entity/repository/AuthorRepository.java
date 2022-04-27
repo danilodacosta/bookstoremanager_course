bcf06c0c737e23b99803b6fe2c4619ea0ef8e6c1
danilodacosta <danilo-gc@hotmail.com>
2022-04-26 23:48:15 -0300

    created repositories

---
+++ src/main/java/com/danilodacosta/bookstoremanager/author/entity/repository/AuthorRepository.java
@@ -0,0 +1,9 @@
+package com.danilodacosta.bookstoremanager.author.entity.repository;
+
+import com.danilodacosta.bookstoremanager.author.entity.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuthorRepository extends JpaRepository<Author, Long> {
+}
