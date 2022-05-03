76baa0f8ad53dc81be16e923bd168a080e42a214
danilodacosta <danilo-gc@hotmail.com>
2022-05-03 01:09:40 -0300

    Created base authors structure classes

---
+++ src/main/java/com/danilodacosta/bookstoremanager/author/controller/AuthorControllerDocs.java
@@ -0,0 +1,8 @@
+package com.danilodacosta.bookstoremanager.author.controller;
+
+import io.swagger.annotations.Api;
+
+@Api("Authors management")
+public interface AuthorControllerDocs {
+
+}
---
+++ src/main/java/com/danilodacosta/bookstoremanager/author/mapper/AuthorMapper.java
@@ -0,0 +1,17 @@
+package com.danilodacosta.bookstoremanager.author.mapper;
+
+import com.danilodacosta.bookstoremanager.author.dto.AuthorDTO;
+import com.danilodacosta.bookstoremanager.author.entity.Author;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface AuthorMapper {
+
+    AuthorMapper INSTANCE = Mappers.getMapper(AuthorMapper.class);
+
+    Author toModel(AuthorDTO authorDTO);
+
+    AuthorDTO toDTO(Author author);
+
+}
